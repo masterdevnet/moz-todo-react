6a52a958dba0f158edb9977430f668dd58c2f168
masterdevnet <masterdev.net@gmail.com>
2021-11-08 11:48:00 -0300

    Inicializando repositório do projeto

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+function App(props) {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="todoapp stack-large">
+      <h1>TodoMatic</h1>
+      <form>
+        <h2 className="label-wrapper">
+          <label htmlFor="new-todo-input" className="label__lg">
+            O que precisa ser feito?
+          </label>
+        </h2>
+
+        <input 
+          type="text" 
+          id="new-todo-list" 
+          className="input input__lg" 
+          name="text" autoComplete="off" 
+        />
+        <button type="submit" className="btn btn__primary btn__lg">
+          Adicionar
+        </button>
+      </form>
+
+      <div className="filter btn-group stack-exception">
+        <button type="button" className="btn toggle-btn" aria-pressed="true">
+          <span className="visually-hidden">Show </span>
+          <span>Todos</span>
+        </button>
+        <button type="button" className="btn toggle-btn" aria-pressed="false">
+          <span className="visually-hidden">Show </span>
+          <span>Ativa</span>
+        </button>
+        <button type="button" className="btn btn-toggle" aria-pressed="false">
+          <span className="visually-hidden">Show </span>
+          <span className="visually-hidden">tarefas </span>
+          <span>completada</span>
+        </button>
+      </div>
+      <h2 id="list-heading">
+        3 tarefas restantes
+      </h2>
+      <ul role="list" 
+        className="todo-list stack-larger stack-exceptiion"
+        arioa-aria-labelledby="list-heading"
+      >
+        <li className="todo stack-small"></li>
+      </ul>
     </div>
-  );
+  )
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
+ReactDOM.render(<App subject="Alice" />, document.getElementById('root')
+  /* { <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>, } */
+  
 );
 
 // If you want to start measuring performance in your app, pass a function
